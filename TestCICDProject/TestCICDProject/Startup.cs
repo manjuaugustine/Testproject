5761086597fc5751613edcf6bed866737cdd4fec
manjuaugustine <maugustine@suyati.com>
2018-08-21 17:08:29 +0530

    New project

---
+++ TestCICDProject/TestCICDProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCICDProject.Startup))]
+namespace TestCICDProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
